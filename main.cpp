9311bbe891e883903595753a259381a93aaff5f7
SamuelRuizVargas <Samuel.ruizv@udea.edu.co>
2020-10-22 09:09:00 -0500

    Descuido intencional prueba

--- main.cpp
+++ main.cpp
@@ -12,5 +12,13 @@ int main()
     {
         cout <<a[var] << endl;
     }
+    cout<<"Imprimir solo los numeros de la cadena original"<<endl;
+    for(int var=0;var<TAM;var++)
+    {
+        if(a[var]>='0' and a[var]<='9')
+        {
+            cout<<a[var]<<endl;
+        }
+    }
     return 0;
 }
